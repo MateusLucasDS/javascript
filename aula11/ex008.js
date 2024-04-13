26fa941c8ad101276dbaa8da3ed822eb00939638
Mateus Lucas <163222776+MateusLucasDS@users.noreply.github.com>
2024-04-13 00:39:12 -0300

    exercicios 8, 9 e 10

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60
+console.log(`sua velocidade é de ${vel}km/h`)
+if (vel >= 60) { //condição simples
+    console.log('você ultrapassou a velocidade maxima, MULTADO!!')
+}
+console.log('sempre use cinto de segurança!')
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'Brasil'
+console.log(`você vive em ${pais}`)
+if (pais == 'Brasil') {
+    console.log('você é brasileiro')
+}else{
+    console.log('você é estrangeiro')
+}
